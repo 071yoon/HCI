de8c35e72422c6175a0d72fdc0135dd6f8c3a21a
071yoon <yeonggilol@gmail.com>
2022-06-02 15:16:01 +0900

    프리티어 적용

--- screens/RecordScreen.tsx
+++ screens/RecordScreen.tsx
@@ -1,8 +1,8 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
-import React from 'react';
-import { Text, View } from '../components/Themed';
-import Daewha from '../components/daewha';
-import Gwansim from '../components/gwansim';
+import { StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { Text, View } from "../components/Themed";
+import Daewha from "../components/daewha";
+import Gwansim from "../components/gwansim";
 
 export default function RecordScreen({ navigation }: any) {
   const [toggle, setToggle] = React.useState(true);
@@ -12,69 +12,70 @@ export default function RecordScreen({ navigation }: any) {
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={toggleState} style={styles.button}>
-        {toggle? <View style={styles.toggledButton}/> : <View style={styles.untoggleButton} /> }
+        {toggle ? (
+          <View style={styles.toggledButton} />
+        ) : (
+          <View style={styles.untoggleButton} />
+        )}
         <Text style={styles.textsL}>대화 목록</Text>
         <Text style={styles.textsR}>관심 목록</Text>
-        
       </TouchableOpacity>
-      {toggle? <Gwansim /> : <Daewha /> }
+      {toggle ? <Gwansim /> : <Daewha />}
     </View>
-    
   );
 }
 
-
 const styles = StyleSheet.create({
   textsL: {
-    position: 'absolute',
+    position: "absolute",
     fontSize: 18,
     top: 5,
     left: 50,
   },
   textsR: {
-    position: 'absolute',
+    position: "absolute",
     fontSize: 18,
     top: 5,
     right: 50,
   },
   button: {
-    borderColor: '#86CF3C',
+    borderColor: "#86CF3C",
     borderWidth: 2,
-    position: 'absolute',
+    position: "absolute",
     top: 30,
-    width: '80%',
-    overflow: 'hidden',
+    width: "80%",
+    overflow: "hidden",
     height: 40,
     borderRadius: 30,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   untoggleButton: {
-    position: 'absolute',
-    width: '50%',
+    position: "absolute",
+    width: "50%",
     height: 40,
-    overflow: 'hidden',
-    backgroundColor: '#86CF3C',
+    overflow: "hidden",
+    backgroundColor: "#86CF3C",
   },
   toggledButton: {
-    position: 'absolute',
-    width: '50%',
+    position: "absolute",
+    width: "50%",
     right: 0,
     height: 40,
-    overflow: 'hidden',
-    backgroundColor: '#86CF3C',
+    overflow: "hidden",
+    backgroundColor: "#86CF3C",
   },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
